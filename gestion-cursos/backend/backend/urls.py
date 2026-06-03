3a060afa600d6d7e06978105a4011a1412da6440
Maricela <nelly.cruz@tecsup.edu.pe>
2026-06-03 15:06:32 -0500

    Forzar servicio de media en produccion

--- gestion-cursos/backend/backend/urls.py
+++ gestion-cursos/backend/backend/urls.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve # <--- 1. IMPORTA ESTO
+from django.urls import re_path # <--- 2. IMPORTA ESTO
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('cursos.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+    # 3. ESTA RUTA FORZARÁ A RENDER A MOSTRAR LAS IMÁGENES SIEMPRE:
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
